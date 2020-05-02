aa824909371ac5b1ae80fb0f53f133ebf3100a8d
Alaeddine Messaoudi <>
2020-05-02 14:48:12 +0200

    Migrate to swift 5

--- Source/TransitionButton.swift
+++ Source/TransitionButton.swift
@@ -230,7 +230,7 @@ public enum StopAnimationStyle {
 
 
 public extension UIImage {
-    public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+    convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
         let rect = CGRect(origin: .zero, size: size)
         UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
         color.setFill()
